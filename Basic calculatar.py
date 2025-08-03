d48fcbff5bcd3a62bc311b5732bfb8efb428c1be
NJAMBINGOTHO <rosenngotho@gmail.com>
2025-08-03 22:23:42 +0300

    Add files via upload

---
+++ Basic calculatar.py
@@ -0,0 +1,10 @@
+# Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Ask the user for the operation
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform the c
